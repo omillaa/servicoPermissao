a281ee93395838dc9c6b549c88e70edbb112a23b
laripsilva25 <117595456+laripsilva25@users.noreply.github.com>
2024-11-19 21:28:44 -0300

    crudSystems

--- src/rotas/systems.js
+++ src/rotas/systems.js
@@ -1,13 +1,73 @@
-// src/routes/systems.js
 const express = require('express');
+const { poolPromise, sql } = require('../services/db');  
 const router = express.Router();
-//const { supabase } = require('../supa/supabase');  // Importa a instância do supabase
 
-// Listar todos os sistemas
+
+router.post('/', async (req, res) => {
+  const { name } = req.body;
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .input('name', sql.NVarChar, name) 
+      .query('INSERT INTO systems (name) VALUES (@name)');
+    res.status(201).send('criado ');
+  } catch (err) {
+    console.error('eerrro', err); 
+    res.status(500).send('Erro');
+  }
+});
+
 router.get('/', async (req, res) => {
-  const { data, error } = await supabase.from('systems').select('*');
-  if (error) return res.status(400).send(error.message);
-  res.send(data);
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request().query('SELECT * FROM systems');
+    res.status(200).json(result.recordset);  
+  } catch (err) {
+    console.error('erro', err);
+    res.status(500).send('erro');
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .input('id', sql.Int, id)
+      .input('name', sql.NVarChar, name)
+      .query('UPDATE systems SET name = @name WHERE id = @id');
+
+    if (result.rowsAffected > 0) {
+      res.status(200).send('atualizado com sucesso');
+    } else {
+      res.status(404).send('não encontrado');
+    }
+  } catch (err) {
+    console.error('Erro ', err);
+    res.status(500).send('Erro ');
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .input('id', sql.Int, id)
+      .query('DELETE FROM systems WHERE id = @id');
+
+    if (result.rowsAffected > 0) {
+      res.status(200).send('excluído ');
+    } else {
+      res.status(404).send(' não encontrado');
+    }
+  } catch (err) {
+    console.error('Erro ', err);
+    res.status(500).send('Erro ');
+  }
 });
 
 module.exports = router;
